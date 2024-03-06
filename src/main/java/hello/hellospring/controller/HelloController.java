5c383cca1e947fa915f73fd84ceccf02234d5ec7
azuressu <martina0130@naver.com>
2024-03-06 22:46:47 +0900

    api-responsebody

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,5 +24,32 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+    
+    @GetMapping("hello-string")
+    @ResponseBody // body 부분에 직접 data를 넣어주겠다
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // "hello spring" 뷰 이런거 없고 문자 자체가 날라감
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        // json 방식으로 return 하게 됨
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName (String name) {
+            this.name = name;
+        }
     }
 }
